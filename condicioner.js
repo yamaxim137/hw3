bda3d975cbc9eae3e4d7fd3858778f0b04369a81
yamaxim137 <59343015+yamaxim137@users.noreply.github.com>
2020-01-20 20:35:43 +0300

    копипаст

---
+++ condicioner.js
@@ -0,0 +1,20 @@
+function Condicioner(model, qurentTemperature) {
+	Device.call(this, model); // model - что мы сразу с model работали
+	this._qurentTemperature = qurentTemperature;
+
+	this.plusTemperature = function(){
+		if((this._qurentTemperature < 25) && (this._state == true)){
+			this._qurentTemperature += 2;
+		}
+	};
+
+	this.minusTemperature = function(){
+		if((this._qurentTemperature > -5) && (this._state == true)){
+			this._qurentTemperature -= 2;
+		}
+	};
+
+	this.getQurentTemperature = function(){
+		return this._qurentTemperature;
+	};
+}
